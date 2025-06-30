3bb58b852bb258fa2b0d2a2fddcdfad2660d10b3
Niko Prassas <niko.prassas@zocdoc.com>
2025-06-29 22:41:17 -0400

    Setup prisma

---
+++ prisma/migrations/20250630013222_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Poll_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Option" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "pollId" TEXT NOT NULL,
+    "voteCount" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Option_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Option" ADD CONSTRAINT "Option_pollId_fkey" FOREIGN KEY ("pollId") REFERENCES "Poll"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient, Prisma } from './src/app/generated/prisma';
+
+const prisma = new PrismaClient();
+
+const pollData: Prisma.PollCreateInput[] = [
+  {
+    title: 'What is your favorite color?',
+    question: 'What is your favorite color?',
+    options: {
+      create: [
+        { text: 'Red' },
+        { text: 'Blue' },
+        { text: 'Green' },
+        { text: 'Yellow' },
+      ],
+    },
+  },
+];
+
+async function main() {
+  console.log('Seeding database...');
+  for (const poll of pollData) {
+    await prisma.poll.create({ data: poll });
+  }
+  console.log('Database seeded successfully');
+}
+
+main();
